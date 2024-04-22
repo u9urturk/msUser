1431f1173cb4be422e8f5d8b291e4ceed250e628
u9urturk <u9urturk@gmail.com>
2024-04-23 00:07:07 +0300

    oauth2 included

---
+++ src/main/java/com/msUser/msUser/Controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.msUser.msUser.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.msUser.msUser.Core.Utilities.Results.DataResult;
+import com.msUser.msUser.Core.Utilities.Results.SuccessDataResult;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class AuthController {
+	
+	@GetMapping()
+	public DataResult<String> hello(){
+		return new SuccessDataResult<String>("Hello from secure endpoint");
+	}
+
+}
---
+++ src/main/java/com/msUser/msUser/Core/Utilities/Security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.msUser.msUser.Core.Utilities.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		
+		httpSecurity
+			.csrf()
+			.disable()
+			.authorizeHttpRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.oauth2Login()
+			;
+		
+		
+		return httpSecurity.build();
+	}
+}
